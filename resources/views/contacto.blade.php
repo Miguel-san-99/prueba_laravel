4b076ad534e90de8670d0e9b3ae56ee58de1cc53
Miguel Sanchez <mike.one10@hotmail.com>
2025-02-24 16:23:00 -0600

    Añadiendo vista de inicio y contacto

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Contacto</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h1>Contacto</h1>
+            <form method="get">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Correo electronico</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="mensaje" class="form-label">Mensaje</label>
+                    <textarea class="form-control" id="mensaje" rows="6" name="mensaje"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="categoria" class="form-label">Categoria</label>
+                    <select class="form-select" id="categoria" name="categoria" aria-label=""Default select example>
+                        <option selected>Selecciona una categoria</option>
+                        <option value="contratacion">Contratacion</option>
+                        <option value="personal">Personal</option>
+                        <option value="otro">Otro</option>
+                    </select>
+                </div>
+
+                <input class="form-control input-color " type="submit" value="Enviar">
+            </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inicio', function () {
+    return view('inicio');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
